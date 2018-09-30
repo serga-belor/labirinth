2589bbb7d2a9b0f7ccadc51f4ac3e421c6d44c6a
Sergey Belorusets <serga_belor_dev@outlook.com>
2018-09-30 22:07:03 +0300

    add "get" function in labirinth module

--- express.js
+++ express.js
@@ -1,6 +1,15 @@
 /* eslint-env node */
 
 const path = require("path");
-module.exports = function(express, app, req, res) {
-	res.sendFile( path.resolve(__dirname, "./labirinth.html") );
+module.exports = {
+	/**
+	 * 
+	 * @param {Object} express host express object
+	 * @param {Object} app host express instance 
+	 * @param {Object} req request express object
+	 * @param {Object} res response express obgect
+	 */
+	get: function( express, app, req, res ) {
+		res.sendFile( path.resolve(__dirname, "./labirinth.html") );
+	}
 };
