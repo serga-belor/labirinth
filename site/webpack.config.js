117abdd01308189f6fba90a0066cd1e545364ca4
Sergei Belorusets <serga_belor_dev@outlook.com>
2024-08-24 20:10:05 -0400

    add source maps for debugging

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
+    devtool: "source-map",
     entry: "./src/index.tsx",
     output: {
         filename: "index.js",
